dc1969a28f8cf2a904dc6056aa5c283c580073a9
KentaHarada06 <haradakenta4622@gmail.com>
2020-11-06 19:52:44 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :state_id,
+      :delivery_charge_burden_id,
+      :delivery_source_id,
+      :delivery_time_id,
+      :selling_price
+    ).merge(user_id: current_user.id)
+  end
+
 end
