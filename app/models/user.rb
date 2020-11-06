414301144a62bf9370cf6557cb2f76479e021a24
KentaHarada06 <haradakenta4622@gmail.com>
2020-11-06 11:00:38 +0900

    バリテーションのオプションによるリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,15 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は、半角英数字混合で入力してください' }
-         validates :nickname, presence: true
-         validates :kanji_first_name, presence: true, format: { with: VALID_KANJI_NAME_REGEX, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
-         validates :kanji_last_name, presence: true, format: { with: VALID_KANJI_NAME_REGEX, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
-         validates :kana_first_name, presence: true, format: { with: VALID_KANA_NAME_REGEX, message: 'は、全角（カタカナ）で入力してください' }
-         validates :kana_last_name, presence: true, format: { with: VALID_KANA_NAME_REGEX, message: 'は、全角（カタカナ）で入力してください' }
-         validates :birthdate, presence: true
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は、半角英数字混合で入力してください' }
+  with_options presence:true do
+    validates :nickname
+    validates :kanji_first_name, format: { with: VALID_KANJI_NAME_REGEX, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :kanji_last_name, format: { with: VALID_KANJI_NAME_REGEX, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :kana_first_name, format: { with: VALID_KANA_NAME_REGEX, message: 'は、全角（カタカナ）で入力してください' }
+    validates :kana_last_name, format: { with: VALID_KANA_NAME_REGEX, message: 'は、全角（カタカナ）で入力してください' }
+    validates :birthdate
+  end
 
 end
