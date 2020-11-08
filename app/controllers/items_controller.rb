cf70f63d90f2b23291984723ec8b21524758353c
KentaHarada06 <haradakenta4622@gmail.com>
2020-11-08 17:46:48 +0900

    未ログイン処理のリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :move_new_user_session, only: [:new]
+
   def index
   end
 
   def new
-    redirect_to new_user_session_path unless user_signed_in?
     @item = Item.new
   end
 
@@ -17,6 +18,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def move_new_user_session
+    redirect_to new_user_session_path unless user_signed_in? 
+  end
 
   def item_params
     params.require(:item).permit(
